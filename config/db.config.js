27c7c3b297f8ddf1bf8c29ce02638479b906a196
don0043 <daniiar.arkabaev@iaau.edu.kg>
2022-03-24 21:35:59 +0600

    changed database to heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('@sequelize/core');
 
 
-const sequelize = new Sequelize('messagedb', 'postgres', 'tetradka2', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize('dfun02src77lu6', 'hyyrjyhumhlcri', 'eafbf6af86970da4121dfc91fb640566de7b87508259f8fc25faf5872b829dba', {
+    host: 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions:{
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }}
 });
 sequelize.sync();
+
 
 module.exports = sequelize;
