2ef18e7e81394193a9799fd70638d8faf0771eb7
don0043 <daniiar.arkabaev@iaau.edu.kg>
2022-03-24 23:03:07 +0600

    package fixes

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const app = express()
-const port = 3001
 const User=require("./model/user");
 const cors = require('cors')
+const https = require("https");
+const fs = require("fs");
+
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+const options = {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert"),
+};
+
 
 app.post('/sms', (req, res) => {
     const user=User.create({name: req.body.name,
-                                email: req.body.email,
-                                message: req.body.message});
+        email: req.body.email,
+        message: req.body.message});
     return res.json(user);
 
 })
 
 
-app.listen(port, () => {
-    console.log(`Message app listening on port ${port}`)
-})
+https.createServer(options, app)
+    .listen(3001, function (req, res) {
+        console.log("Server started at port 3001");
+    });
+
+
 
